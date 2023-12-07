61dcf29fdc970298a7abac1b64228ef832082a51
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2023-12-08 01:17:26 +0700

    Faster sorting algo

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ class jsonObject ():
             self.data = json.load(f)
     def getCleanedData (self, debugMode = 0):
         data = self.data
-
         #"If an offer is available in multiple merchants, only select the closest merchant"
         for offer in data ['offers']:
             if len (offer['merchants'])>1:
@@ -40,61 +39,67 @@ class jsonObject ():
                 if key not in category_Keep_list:
                     offer.pop (key, None)
         return data
+    
     def ValiDate (self, offer):
-        #Heh, pun
-        #step 1: checkin date += 5 (chac la lam ben ngoai luon)
-        #Redundant step 2: Convert offer date from string to Num (Chac khong can lam)
-        #Step 2 Actual: Convert checkin date to "YYYY-MM-DD" format and just simply
-        # do string comparison with offer date
+        #Heh, Validate date
+        #Validate if offer is valid up to the day that the user inputs + 5 days.
+        #Also return false if category is in the banned category list.
+
+        #Check for None in case there is no rule defined
         if (self.categorySkipList != None):
             if (str(offer["category"]) in self.categorySkipList):
-                print ("Rejected, category: ", offer)
                 return False
         if (self.userInput != None):
             if (offer["valid_to"] >= self.userInput) == False:
-                print ("Rejected, valid date: ",offer)
                 return False
-        print ("Accpeted: ",offer)
         return True
+    
     def getBestOffers(self):
-        Data = self.getCleanedData(1)
-        #for a in Data['offers']:
-        #    if (self.filterOffer (a) == False):
-        #        Data['offers'].remove (a)
-        nigger = [offer for offer in Data['offers'] if self.ValiDate (offer)]
-        idx = [i for i in range (0, len(nigger))]
+        Data = self.getCleanedData(0)
+
+        #One liner: sorted([offer for offer in Data['offers'] if self.ValiDate (offer)], key=lambda d: d['merchants'][0]['distance'])
+        #Nhung chac chan khong doc duoc roi
+
+        #chua duoc sort, se sort o duoi
+        sortedOffer = [offer for offer in Data['offers'] if self.ValiDate (offer)]
+        idx = [i for i in range (0, len(sortedOffer))]
         #If there are multiple offers in the same category *give priority to the closest merchant offer*.
         #If there are multiple offers with different categories, *select the closest merchant offers when selecting 2 offers*.
-        #Gnomesort, cuz I'm lazy coding this
         pos = 0
-        while (pos < len(idx)):
-            if (pos == 0) or (nigger[idx[pos]]['merchants'][0]['distance'] > nigger[idx[pos-1]]['merchants'][0]['distance']):
-                pos = pos+1
-            else:
-                #swap idx[pos] with idx[pos-1]
-                idx[pos] = idx [pos] + idx[pos-1]
-                idx[pos-1] =  idx [pos] - idx[pos-1]
-                idx [pos] =  idx [pos] - idx[pos-1]
-                pos = pos-1
-                
+        sortedOffer = sorted(sortedOffer, key=lambda d: d['merchants'][0]['distance'])
+        #Gnomesort, cuz I'm lazy coding this
+        #while (pos + 1 < len(idx)):
+        #    if (pos == 0) or (sortedOffer[idx[pos]]['merchants'][0]['distance'] > sortedOffer[idx[pos-1]]['merchants'][0]['distance']):
+        #        pos = pos+1
+        #    else:
+        #        #swap idx[pos] with idx[pos-1]
+        #        idx[pos] = idx [pos] + idx[pos-1]
+        #        idx[pos-1] =  idx [pos] - idx[pos-1]
+        #        idx [pos] =  idx [pos] - idx[pos-1]
+        #        pos = pos-1
+        #sortedOffer = [sortedOffer[i] for i in range (0, len(idx))]
+        #print (sortedOffer)
         count = 0
         selectedOffers = []
         for i in range (0, len(idx)):
             if (count == self.numOfferNeeded):
                 break
-            
             #"selected offers should be in different categories"
             categoryExistCheck = 0
             for a in selectedOffers:
-                if (a['category'] == nigger[idx[i]]['category']):
+                if (a['category'] == sortedOffer[idx[i]]['category']):
                     categoryExistCheck = 1
                     break
             if (categoryExistCheck == 0):
-                selectedOffers.append (nigger[idx[i]])
+                selectedOffers.append (sortedOffer[idx[i]])
             count = count + 1
-        return selectedOffers
+        result = {'offers':selectedOffers}
+        return result
 
     
 test = jsonObject (userInput, categorySkipList)
+#test = jsonObject ()
 test.readFile (filePath)
-test.getBestOffers()
+result = test.getBestOffers()
+with open('output.json', 'w') as f:
+  json.dump(result, f, ensure_ascii=False, indent=2)
