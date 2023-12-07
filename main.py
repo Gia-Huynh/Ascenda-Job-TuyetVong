5ccf605c4bb74bcd4ca9b2377fdfa479f4366640
GasTheWoods <124686986+Gia-Huynh@users.noreply.github.com>
2023-12-07 20:17:05 +0700

    Add date to user input string
    Done, now compare string literal

--- main.py
+++ main.py
@@ -1,55 +1,36 @@
 import json
-
+import utils
+userInput = "2020-11-10"
 filePath = 'input.json'
-def addDate (Y, M, D, addNum):
-    #Y, M, D = Year, Month, Day
-    #addNum = number of days to add
-    
-    def getDayInMonth ():
-        month = M
-        year = Y
-        Days_In_Month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        if (month!=2):
-            return Days_In_Month [month]
-        elif (year % 4 == 0):
-            if (year % 100 == 0):
-                if (year % 400 == 0):
-                    return 29
-                return 28
-            return 29
-        else:
-            return 28
-    while (addNum > 0):
-        #Some optimization can be done here, instead of
-        # adding 1 by 1 to D,
-        # we can add the Day in Month minus 1 straight into D
-        D = D + 1
-        if (D - 1 == getDayInMonth ()):
-            D = 1
-            M = M+1
-            if (M == 13):
-                Y = Y+1
-                M = 1
-        addNum = addNum - 1
-    return Y, M ,D
+print (utils.addDate (userInput, 5))
+
 class jsonObject ():
     def __init__ (self):
         self.data = None
     def getData (self):
         return self.data
-    def cleanData (self, data):
-        #Optional steps, to remove data clutter by removing
-        #unused class.
-        Category_Keep_list = ["id", "category", "merchants", "valid_to"]
-        baka = data
-        for a in baka['offers']:
-            for key in a.copy():
-                if key not in self.keep_list:
-                    a.pop (key, None)
-        return a
     def readFile (self, filePath):
         with open(filePath) as f:
             self.data = json.load(f)
-        
+    def getCleanedData (self):
+        #Optional steps, to remove data clutter by removing
+        #unused class.
+        category_Keep_list = ["id", "category", "merchants", "valid_to"]
+        bak_data = self.data
+        for a in bak_data['offers']:
+            for key in a.copy():
+                if key not in category_Keep_list:
+                    a.pop (key, None)
+        return bak_data
+    def filterOffer (offer):
+        #step 1: checkin date += 5 (chac la lam ben ngoai luon)
+        #Redundant step 2: Convert offer date from string to Num (Chac khong can lam)
+        #Step 2 Actual: Convert checkin date to "YYYY-MM-DD" format and just simply
+        # do string comparison with offer date
+        if date_compare (offer["valid_to"], input_date) == True:
+            return 0
+        return 1
+
+    
 test = jsonObject ()
 test.readFile (filePath)
---
+++ utils.py
@@ -0,0 +1,35 @@
+def addDate (stringInput, addNum):
+    def addDate (Y, M, D, addNum):
+        #Y, M, D = Year, Month, Day
+        #addNum = number of days to add
+        def getDayInMonth ():
+            Days_In_Month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+            if (M!=2):
+                return Days_In_Month [M]
+            elif (Y % 4 == 0):
+                if (Y % 100 == 0):
+                    if (Y % 400 == 0):
+                        return 29
+                    return 28
+                return 29
+            else:
+                return 28
+        while (addNum > 0):
+            #Some optimization can be done here, instead of
+            # adding 1 by 1 to D,
+            # we can add the Day in Month minus 1 straight into D
+            D = D + 1
+            if (D - 1 == getDayInMonth ()):
+                D = 1
+                M = M+1
+                if (M == 13):
+                    Y = Y+1
+                    M = 1
+            addNum = addNum - 1
+        return Y, M ,D
+    Y = int(stringInput.split('-')[0])
+    M = int(stringInput.split('-')[1])
+    D = int(stringInput.split('-')[2])
+    Y, M, D = addDate (Y,M,D,addNum)
+    result = f'{Y:04d}-{M:02d}-{D:02d}'
+    return result
